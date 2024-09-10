354b7d72f107d8dd8ad83e830749cf96f0fffb3c
evelynvic23 <evelynvic2310@gmail.com>
2024-09-10 16:51:55 -0300

    feat(App class) Criando conexão de código

--- FazerVsCode/src/App.java
+++ FazerVsCode/src/App.java
@@ -3,29 +3,24 @@ import java.util.Scanner;
 public class App {
         //Comentário
 
-    public static void funcao_teste (){
-        //Biblioteca para entrada de dados
-        Scanner ler = new Scanner(System.in);
 
-        //variável local 
-        String v2;
-        int v1;
+public static void main(String[] args){
 
-        
+    // Criação de Instância
 
-        System.out.println("Comando de Saida de Dados");
-        
-        System.out.printf("Informe uma palavra: ");
-        v2 = ler.next();
+    RA2920482411042 rainstancia = new RA2920482411042();
 
-        System.out.printf("Informe um número: ");
-        v1 = ler.nextInt();
-
-        System.out.printf(v2+" "+v1);
-    }
-    public static void main(String[] args) throws Exception {
-        funcao_teste();
-
-
-    }
+    System.out.println("Nome completo: " + rainstancia.nome);
+    System.out.println("RA: " + rainstancia.ra);
+    System.out.println("Idade: " + rainstancia.idade);
+    System.out.println("GitHub: " + rainstancia.git);
+    System.out.println("Linkedln: " + rainstancia.linkedln);
+    System.out.println("Curso: " + rainstancia.curso);
+    System.out.println("Turma: " + rainstancia.turma);
+   
 }
+
+
+}
+
+// Criando a Classe com meu RA
